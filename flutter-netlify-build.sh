9a800fe95b0b1e119839574e6c3ede4960258f48
hammadsyr <hammad.syarif@work.bri.co.id>
2022-07-19 19:17:09 +0700

    updating netlify build

--- flutter-netlify-build.sh
+++ flutter-netlify-build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-FLUTTER_BRANCH=`grep channel: .metadata | sed 's/  channel: //g'`
-FLUTTER_REVISION=`grep revision: .metadata | sed 's/  revision: //g'`
+# FLUTTER_BRANCH=`grep channel: .metadata | sed 's/  channel: //g'`
+# FLUTTER_REVISION=`grep revision: .metadata | sed 's/  revision: //g'`
 
 git clone https://github.com/flutter/flutter.git
 cd flutter
-git checkout $FLUTTER_BRANCH
-git pull origin $FLUTTER_BRANCH
-git checkout $FLUTTER_REVISION
+# git checkout $FLUTTER_BRANCH
+# git pull origin $FLUTTER_BRANCH
+# git checkout $FLUTTER_REVISION
 cd ..
 
 flutter/bin/flutter config --enable-web
-flutter/bin/flutter build web
+flutter/bin/flutter build web --release
